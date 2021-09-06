d821d242ff8f2e65921052341ae26a093a1c0d6f
RakeshLonikarOfficeWohlig <88829396+RakeshLonikarOfficeWohlig@users.noreply.github.com>
2021-09-06 18:19:24 +0530

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <nav>
+      <v-toolbar flat app>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title class="text-uppercase grey--text">
+          <span class="font-weight-light">Todo</span>
+          <span>Ninja</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn depressed color="white">
+          <span>Sing Out</span>
+          <v-icon right>mdi-export</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-navigation-drawer app v-model="drawer" absolute bottom temporary>
+        <v-list>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-tile
+              v-for="link in links"
+              :key="link.text"
+              router
+              :to="link.route"
+            >
+              <v-list-item>
+                <v-list-item-title>{{ link.text }}</v-list-item-title>
+              </v-list-item>
+            </v-list-tile>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { text: "Dashboard", route: "/" },
+        { text: "Project", route: "/project" },
+        { text: "Team", route: "/team" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Team.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="team">
+    <hello-world />
+    <h1>Team</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Team",
+};
+</script>
+
